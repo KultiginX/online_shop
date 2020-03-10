70d8accf6830e36248fc4805af4e3a19c4a99887
KultiginX <kbozdemir@gmail.com>
2020-03-10 17:16:28 +0100

    Update online_shopping.R

--- online_shopping.R
+++ online_shopping.R
@@ -2,18 +2,39 @@
 
 
 getwd()
-setwd("/Users/kultiginbozdemir/GitHub/online_shop")
+#setwd("/Users/kultiginbozdemir/GitHub/online_shop")
+rstudioapi::getSourceEditorContext()$path
+
 df<-read.delim2("iw_customer.txt")
-head(df)
+str(df)
 summary(df)
 head(df)
 tail(df)
-bo(df$birthdate)
+
 min(as.Date(df$birthdate))
 summary(df$birthdate)
-df<- data.frame(df)
 df$birthdate<-as.Date(df$birthdate)
 str(df$birthdate)
 min(df$birthdate)
-df$age<- as.integer((Sys.Date()-df$birthdate)/365)
+# we add a new column of age
+df$age<- as.integer((Sys.Date()-df$birthdate)/365) # not very accurate, it works for now
 head(df$age)
+#let's drop unneccessary columns
+drops <- c("riskID","eMail", "newsletter",  "owner",
+           "creditLimit", "street","firstname","surname","birthdate")
+df<-df[ , !(names(df) %in% drops)]
+head(df)
+#Create a new column of gender
+summary(df$salutation)
+df$gender<-ifelse ( df$salutation=="Frau", "Female", "Male" )
+#df[,c("salutation","gender")]
+df$gender<-as.factor(df$gender)
+#Chech if we created the new gender column correctly.
+summary(df$gender)
+summary(df$salutation)
+head(df)
+drops <- c("salutation")
+df<-df[ , !(names(df) %in% drops)]
+head(df)
+summary(df)
+boxplot(df[,2:4])
